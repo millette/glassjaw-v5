7d7c409a0022b1c818b49d0d68b9edcf2c06f882
Robin Millette <robin@millette.info>
2018-06-27 05:04:02 -0400

    Confirm punch on page change.

--- shared/components/habit.js
+++ shared/components/habit.js
@@ -3,22 +3,6 @@
 import React, { Component } from 'react'
 import setupFSM from '../utils/habit-fsm'
 
-/*
-FIXME: stop the fsm
-Warning: Can't call setState (or forceUpdate) on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.
-    in Habit (at habits.js:16)
-    in div (at habits.js:14)
-    in Habits (at punch.js:35)
-    in div (at punch.js:34)
-    in div (at punch.js:33)
-    in div (at punch.js:30)
-    in section (at punch.js:29)
-    in Punch (created by Connect(Punch))
-    in Connect(Punch) (created by ComponentWithRematch)
-    in Provider (created by ComponentWithRematch)
-    in ComponentWithRematch (created by App)
-*/
-
 const titles = {
   'is-info': 'Click to punch',
   'is-warning': 'Click to undo',
@@ -31,7 +15,9 @@ class Habit extends Component {
     this.onSubmitDefault = this.onSubmitDefault.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.state = { ggg: '', messageType: 'is-info' }
-    this.clickFSM = setupFSM(this)
+    const { click, stop } = setupFSM(this)
+    this.clickFSM = click
+    this.componentWillUnmount = stop
   }
 
   punch () { this.props.add({ ggg: this.state.ggg, i: this.props.i }) }
--- shared/utils/habit-fsm.js
+++ shared/utils/habit-fsm.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// import Finity, { configure } from 'finity'
 import { configure } from 'finity'
 
 const setupFSM = (self, timeout = 30) => {
@@ -11,9 +12,19 @@ const setupFSM = (self, timeout = 30) => {
   }
 
   const confirm = () => {
-    self.punch()
     clearInterval(timer)
+    self.punch()
     self.setState({ countdown: false, ggg: '', messageType: 'is-success' })
+  }
+
+  const stop = (a, b, c) => {
+    clearInterval(timer)
+    c.stateMachine.currentStateMachine.stop()
+  }
+
+  const unmount = (a, b, c) => {
+    self.punch()
+    stop(a, b, c)
   }
 
   const punched = () => {
@@ -25,18 +36,29 @@ const setupFSM = (self, timeout = 30) => {
     }, 1000)
   }
 
+  // const wrkr1 = configure()
   const wrkr = configure()
     .initialState('ready')
     .on('click').transitionTo('punched').withAction(punched)
+    .on('componentWillUnmount').internalTransition()
     .state('punched')
     .on('click').transitionTo('ready').withAction(ready)
     .onTimeout(timeout * 1000).transitionTo('confirmed').withAction(confirm)
+    .on('componentWillUnmount').internalTransition().withAction(unmount)
     .state('confirmed')
     .on('click').transitionTo('ready').withAction(ready)
     .onTimeout(3000).transitionTo('ready').withAction(ready)
+    .on('componentWillUnmount').internalTransition().withAction(stop)
+    // .global()
+    // .onStateExit((state) => console.log(`Exiting state '${state}'`))
+    // .getConfig()
     .start()
 
-  return wrkr.handle.bind(wrkr, 'click')
+  // const wrkr = Finity.start(wrkr1)
+  return {
+    click: wrkr.handle.bind(wrkr, 'click'),
+    stop: wrkr.handle.bind(wrkr, 'componentWillUnmount')
+  }
 }
 
 export default setupFSM
