f71d3c92612f486c31110d190a8fce3491873af8
Robin Millette <robin@millette.info>
2018-06-27 00:30:34 -0400

    Added todo (fixme).

--- shared/components/habit.js
+++ shared/components/habit.js
@@ -3,6 +3,22 @@
 import React, { Component } from 'react'
 import setupFSM from '../utils/habit-fsm'
 
+/*
+FIXME: stop the fsm
+Warning: Can't call setState (or forceUpdate) on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.
+    in Habit (at habits.js:16)
+    in div (at habits.js:14)
+    in Habits (at punch.js:35)
+    in div (at punch.js:34)
+    in div (at punch.js:33)
+    in div (at punch.js:30)
+    in section (at punch.js:29)
+    in Punch (created by Connect(Punch))
+    in Connect(Punch) (created by ComponentWithRematch)
+    in Provider (created by ComponentWithRematch)
+    in ComponentWithRematch (created by App)
+*/
+
 const titles = {
   'is-info': 'Click to punch',
   'is-warning': 'Click to undo',
