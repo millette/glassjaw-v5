fff9361534d53e4a6cd411e8379b4a214129149d
Robin Millette <robin@millette.info>
2018-06-27 23:02:09 -0400

    More text on frontpage and about.

--- pages/about.js
+++ pages/about.js
@@ -10,17 +10,54 @@ const About = () => <Fragment>
   <section className='section'>
     <div className='container'>
       <div className='content'>
+        <p>
+          Vous voulez <b>prendre de bonnes habitudes</b> et
+          vous <b>débarrasser des mauvaises</b>? Ce service est pour vous!
+        </p>
         <div className='columns'>
           <div className='column'>
-            <h2>Prototype</h2>
+            <h3 className='title is-3'>Inscription</h3>
             <p>
-              Fee
+              L’inscription au site est gratuite et prend 30 secondes.
+              La seule information exigée est un nom d’utilisateur
+              et un mot de passe pour vous connecter.
             </p>
           </div>
           <div className='column'>
-            <h2>Asrtola</h2>
+            <h3 className='title is-3'>Créer un premier <i>punch</i></h3>
             <p>
-              Fooo
+              Une fois connecté, vous pouvez créer votre premier <i>punch</i>.
+              Un <i>punch</i> est une habitude que vous voulez mieux gérer.
+            </p>
+          </div>
+          <div className='column'>
+            <h3 className='title is-3'>Puncher</h3>
+            <p>
+              Quand l’habitude se présente, il suffit de <i>puncher</i> l’habitude.
+              Vous pouvez gérer plusieurs habitudes, bien sûr.
+            </p>
+          </div>
+        </div>
+        <div className='columns'>
+          <div className='column'>
+            <h3 className='title is-3'>Vie privée</h3>
+            <p>
+              Toutes les informations demeurent privées et seulement accessibles
+              au détenteur du compte.
+            </p>
+          </div>
+          <div className='column'>
+            <h3 className='title is-3'>Nous contacter</h3>
+            <p>
+              Utilisez le formulaire pour nous contacter ou envoyez nous un email.
+            </p>
+          </div>
+          <div className='column'>
+            <a target='_blank' href='https://github.com/millette/glassjaw-v5'>
+              <h3 className='title is-3'>Crédits</h3>
+            </a>
+            <p>
+              Les sources du logiciel utilisé par ce service sont disponibles sur GitHub.
             </p>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,10 @@ import withRematch from '../shared/utils/withRematch'
 
 const Home = () => <Fragment>
   <Header title='Accueil' subtitle='Prototype'>
-    <p>
-      <i>
-        <b>ATTENTION</b> Pour tester ce site, ne rechargez pas la page
-        à moins de vouloir remettre les valeurs à neufs.
-        Les données ne sont ni transmises, ni sauvegardées.
-      </i>
+    <p className='is-italic'>
+      <b>ATTENTION</b> Pour tester ce site, ne rechargez pas la page
+      à moins de vouloir remettre les valeurs à neufs.
+      Les données ne sont ni transmises, ni sauvegardées.
     </p>
     <p>
       <b>Il ne s’agit que d’une démonstration éphémère</b>.
@@ -20,15 +18,10 @@ const Home = () => <Fragment>
   </Header>
   <section className='section'>
     <div className='container'>
-      <div className='content'>
-        <div className='columns'>
-          <div className='column'>
-            <p>
-              Bonjour.
-            </p>
-          </div>
-        </div>
-      </div>
+      <p>
+        Vous voulez <b>prendre de bonnes habitudes</b> et
+        vous <b>débarrasser des mauvaises</b>? Ce service est pour vous!
+      </p>
     </div>
   </section>
 </Fragment>
